7ea5f115255292bff4c1a024383093dcbbe91b03
Yawar <bhatsharik6146@gmail.com>
2021-11-18 20:26:07 +0530

    Added new code in Main.java file

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main{
     public static void main(String[] args) {
-
+   int num =12345;
+        System.out.println(sum(num));
+    }
+    public static  int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        int s =0;
+        int rem= n%10;
+        s*=10+rem;
+        sum(n/10);
     }
 }
